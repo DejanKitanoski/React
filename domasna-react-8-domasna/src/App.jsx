0c7bd7a378061503ed02d779b84540b389a93a29
Dejan <supbro606@gmail.com>
2025-04-23 18:32:02 +0200

    Domasna

--- domasna-react-8-domasna/src/App.jsx
+++ domasna-react-8-domasna/src/App.jsx
@@ -1,13 +1,15 @@
+
 import TodoList from './components/todoList'
 import './App.css'
-
+import TodoProvider from './components/TodoContext'
+import ToDoList from './components/todoList'
 function App() {
   
 
   return (
-    <div>
+    <TodoProvider>
       <TodoList/>
-    </div>
+    </TodoProvider>
       
   )
 }
---
+++ domasna-react-8-domasna/src/components/TodoContext.jsx
@@ -0,0 +1,62 @@
+
+
+import { useState, createContext } from "react";
+
+
+export const ToDoContext = createContext()
+
+
+function TodoProvider({children}) {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (!newTask.trim()) return;
+    setTasks((t) => [...t, newTask.trim()]);
+    setNewTask("");
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+ 
+  }
+
+  function startEdit(index) {
+    setEditIndex(index);
+    setEditText(tasks[index]);
+  }
+
+  function saveEdit() {
+   
+    const updatedTasks = [...tasks];
+    updatedTasks[editIndex] = editText.trim();
+    setTasks(updatedTasks);
+    setEditIndex(null);
+    setEditText("");
+  }
+  return(
+    <ToDoContext.Provider value={{
+        tasks,
+        newTask,
+        editIndex,
+        editText,
+        handleInputChange,
+        addTask,
+        deleteTask,
+        startEdit,
+        saveEdit,
+        setEditText,
+      }}>
+        {children}
+      </ToDoContext.Provider>
+  )
+}
+
+export default TodoProvider
